c6b328152015d93bd3146d995f8e6fc6ebd5d331
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2021-12-08 17:48:20 -0300

    separando os arquivos para ficar um pouco mais organizado

---
+++ src/enumeracoesEcomposicao/AprendendoEnumeracoes.java
@@ -0,0 +1,26 @@
+package enumeracoesEcomposicao;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class AprendendoEnumeracoes {
+
+	public static void main(String[] args) {
+		
+		// enum serve como constantes
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PEDING_PAYMENT);
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+
+	}
+
+}

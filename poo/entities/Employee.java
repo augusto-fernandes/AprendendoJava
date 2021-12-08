5bfdf0ba05f6ec16494158259b72ba28cb5b43c1
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2021-12-08 18:53:21 -0300

    organizacao finalizada

---
+++ poo/entities/Employee.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Employee {
+	public String name;
+	public double GrossSalary;
+	public double tax;
+	
+	public double netSalary(){
+		return GrossSalary - tax;
+	}
+	 public void increaseSalary(double percentage) {
+		 GrossSalary += GrossSalary * percentage /100; 
+	 }
+	 public String toString() {
+		 return name + ", $ " + String.format("%.2f", netSalary());
+		 }
+	}
+
+

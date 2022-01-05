8e787a2866a43af8b82cea466427143c2e008f95
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-01-05 17:09:45 -0300

    começando o modulo sobre interface

--- interfaces/model/services/BrazilTaxService.java
+++ interfaces/model/services/BrazilTaxService.java
@@ -4,9 +4,12 @@ public class BrazilTaxService {
 	//calculando o imposto do brasil
 	
 	public Double tax(double amount) {
-		// o impostoé 20% pra quantias até 100 reais e 15% pra valor acima disso.
+		// o imposto é 20% pra quantias até 100 reais e 15% pra valor acima disso.
 		if(amount<= 100.0) {
 			return amount * 0.2;
+		}
+		else {
+			return amount *0.15;
 		}
 		
 	}
